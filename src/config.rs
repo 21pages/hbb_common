0801da77b444ba5d9007505a9954577d3ba09666
21pages <sunboeasy@gmail.com>
2026-07-15 19:26:52 +0800

    disable-installation
    Signed-off-by: 21pages <sunboeasy@gmail.com>

--- src/config.rs
+++ src/config.rs
@@ -2820,7 +2820,7 @@ pub fn is_disable_account() -> bool {
 
 #[inline]
 pub fn is_disable_installation() -> bool {
-    is_some_hard_opton("disable-installation")
+    true
 }
 
 // This function must be kept the same as the one in flutter and sciter code.
